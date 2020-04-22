a934940f16db01c44e4b74006c497b1e8ae7ae05
Ceren Ugurlu <cerenugurlu8l@gmail.com>
2020-04-22 17:14:57 +0200

    Update DatabaseConnection.java

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -20,6 +20,7 @@ public class DatabaseConnection {
      * @return Connection. Returns the connection it is made.
      * @throws SQLException when there is no connection
      */
+    public static Connection getConnection() throws SQLException {
         Connection conn;
         conn = DriverManager.getConnection(url, user, password);
         return conn;
